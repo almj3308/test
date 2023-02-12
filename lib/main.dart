a43be157f890fbc954cc41362d90738637e39a66
MJ <almj3308@gmail.com>
2023-02-12 17:21:39 +0900

    Init

--- lib/main.dart
+++ lib/main.dart
@@ -1,115 +1,182 @@
-import 'package:flutter/material.dart';
+import 'dart:async';
+import 'dart:math';
 
-void main() {
-  runApp(const MyApp());
+import 'package:flutter/material.dart';
+import 'package:wallpaper/wallpaper.dart';
+
+void main() => runApp(MaterialApp(debugShowCheckedModeBanner: false, home: MyApp()));
+
+class MyApp extends StatefulWidget {
+  @override
+  _MyAppState createState() => _MyAppState();
 }
 
-class MyApp extends StatelessWidget {
-  const MyApp({super.key});
+class _MyAppState extends State<MyApp> {
+  String home = "Home Screen", lock = "Lock Screen", both = "Both Screen", system = "System";
 
-  // This widget is the root of your application.
+  late Stream<String> progressString;
+  late String res;
+  bool downloading = false;
+  List<String> images = [
+    // "wallpaper01.gif",
+    // "wallpaper02.jpg"
+    "https://cdn.pixabay.com/photo/2019/12/31/05/54/quokka-4731259_960_720.jpg",
+    // "https://images.pexels.com/photos/10069890/pexels-photo-10069890.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500",
+    // "https://images.pexels.com/photos/7037125/pexels-photo-7037125.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500",
+    // "https://images.pexels.com/photos/8803905/pexels-photo-8803905.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500",
+    // "https://images.pexels.com/photos/9556451/pexels-photo-9556451.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500",
+    // "https://images.pexels.com/photos/10050591/pexels-photo-10050591.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500",
+    // "https://images.pexels.com/photos/9000160/pexels-photo-9000160.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500",
+    // "https://images.pexels.com/photos/9676202/pexels-photo-9676202.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500",
+    // "https://images.pexels.com/photos/9308054/pexels-photo-9308054.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500"
+  ];
+  var result = "Waiting to set wallpaper";
+  bool _isDisable = true;
+
+  int nextImageID = 0;
+
+  @override
+  void initState() {
+    super.initState();
+  }
+
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // Try running your application with "flutter run". You'll see the
-        // application has a blue toolbar. Then, without quitting the app, try
-        // changing the primarySwatch below to Colors.green and then invoke
-        // "hot reload" (press "r" in the console where you ran "flutter run",
-        // or simply save your changes to "hot reload" in a Flutter IDE).
-        // Notice that the counter didn't reset back to zero; the application
-        // is not restarted.
-        primarySwatch: Colors.blue,
-      ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+    return Scaffold(
+      body: Container(
+          margin: EdgeInsets.only(top: 20),
+          width: MediaQuery.of(context).size.width,
+          height: MediaQuery.of(context).size.height,
+          child: SingleChildScrollView(
+            child: Column(
+              children: <Widget>[
+                downloading
+                    ? imageDownloadDialog()
+                    // : Image.asset(
+                    //     'assets/wallpaper/${images[nextImageID]}',
+                    //     fit: BoxFit.fitWidth,
+                    //   ),
+                    : Image.network(
+                        images[nextImageID],
+                        fit: BoxFit.fitWidth,
+                      ),
+                ElevatedButton(
+                  onPressed: () async {
+                    setState(() {
+                      nextImageID = Random().nextInt(images.length);
+                      _isDisable = true;
+                    });
+                  },
+                  child: Text("Get Random Image"),
+                ),
+                ElevatedButton(
+                  onPressed: () async {
+                    return await dowloadImage(context);
+                  },
+                  child: Text("please download the image"),
+                ),
+                ElevatedButton(
+                  onPressed: _isDisable
+                      ? null
+                      : () async {
+                          var width = MediaQuery.of(context).size.width;
+                          var height = MediaQuery.of(context).size.height;
+                          home = await Wallpaper.homeScreen(options: RequestSizeOptions.RESIZE_FIT, width: width, height: height);
+                          setState(() {
+                            downloading = false;
+                            home = home;
+                          });
+                          print("Task Done");
+                        },
+                  child: Text(home),
+                ),
+                ElevatedButton(
+                  onPressed: _isDisable
+                      ? null
+                      : () async {
+                          lock = await Wallpaper.lockScreen();
+                          setState(() {
+                            downloading = false;
+                            lock = lock;
+                          });
+                          print("Task Done");
+                        },
+                  child: Text(lock),
+                ),
+                ElevatedButton(
+                  onPressed: _isDisable
+                      ? null
+                      : () async {
+                          both = await Wallpaper.bothScreen();
+                          setState(() {
+                            downloading = false;
+                            both = both;
+                          });
+                          print("Task Done");
+                        },
+                  child: Text(both),
+                ),
+                ElevatedButton(
+                  onPressed: _isDisable
+                      ? null
+                      : () async {
+                          system = await Wallpaper.systemScreen();
+                          setState(() {
+                            downloading = false;
+                            system = system;
+                          });
+                          print("Task Done");
+                        },
+                  child: Text(system),
+                ),
+              ],
+            ),
+          )),
     );
   }
-}
 
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({super.key, required this.title});
+  Future<void> dowloadImage(BuildContext context) async {
+    progressString = Wallpaper.imageDownloadProgress(images[nextImageID]);
+    progressString.listen((data) {
+      setState(() {
+        res = data;
+        downloading = true;
+      });
+      print("DataReceived: " + data);
+    }, onDone: () async {
+      setState(() {
+        downloading = false;
 
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
-
-  final String title;
-
-  @override
-  State<MyHomePage> createState() => _MyHomePageState();
-}
-
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
-
-  void _incrementCounter() {
-    setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
+        _isDisable = false;
+      });
+      print("Task Done");
+    }, onError: (error) {
+      setState(() {
+        downloading = false;
+        _isDisable = true;
+      });
+      print("Some Error");
     });
   }
 
-  @override
-  Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
-    return Scaffold(
-      appBar: AppBar(
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
-        title: Text(widget.title),
-      ),
-      body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
+  Widget imageDownloadDialog() {
+    return Container(
+      height: 120.0,
+      width: 200.0,
+      child: Card(
+        color: Colors.black,
         child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Invoke "debug painting" (press "p" in the console, choose the
-          // "Toggle Debug Paint" action from the Flutter Inspector in Android
-          // Studio, or the "Toggle Debug Paint" command in Visual Studio Code)
-          // to see the wireframe for each widget.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
           mainAxisAlignment: MainAxisAlignment.center,
           children: <Widget>[
-            const Text(
-              'You have pushed the button this many times:',
-            ),
+            CircularProgressIndicator(),
+            SizedBox(height: 20.0),
             Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headline4,
-            ),
+              "Downloading File : $res",
+              style: TextStyle(color: Colors.white),
+            )
           ],
         ),
       ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
     );
   }
 }
